674b2f20cecac608dad6ea8c60cd9d4b0503ca7e
miker-bice <mikejerrson.galindez@gmail.com>
2022-08-08 15:35:04 +0800

    pokemon GIFs now working | fuckin great

--- pokemon/views.py
+++ pokemon/views.py
@@ -6,14 +6,17 @@ import requests
 # this is the default home page
 def home(request):
     # try doing a call to the API
-    url = "https://pokeapi.co/api/v2/pokemon/pikachu"
+    url = "https://pokeapi.co/api/v2/pokemon/mewtwo"
     res = requests.get(url)
 
     if res.status_code != 200:
         print(res.text)
     else:
         data = res.json()
+        animated_gif = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        print(animated_gif)
         context = {
-            'pokedata':data
+            'pokedata': data,
+            'gifdata': animated_gif,
         }
         return render(request, 'pokemon/home.html', context)
