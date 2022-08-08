5cc4fc1286a12b4ef274c9e3bc65e413870bb181
miker-bice <mikejerrson.galindez@gmail.com>
2022-08-08 22:10:33 +0800

    code clean up

--- pokemon/templatetags/zip.py
+++ pokemon/templatetags/zip.py
@@ -3,5 +3,5 @@ from django import template
 register = template.Library()
 
 @register.filter(name='zip')
-def zip_lists(a, b):
-    return zip(a, b)
+def zip_lists(var1, var2):
+    return zip(var1, var2)
--- pokemon/views.py
+++ pokemon/views.py
@@ -2,40 +2,50 @@ import re
 from django.shortcuts import render
 import requests
 
-# Create your views here.
 
 # this is the default home page
 def home(request):
-    # try doing a call to the API
     # url = "https://pokeapi.co/api/v2/pokemon/mewtwo" #this is for single pokemon API calls
-    batch_url = "https://pokeapi.co/api/v2/pokemon?limit=10" #this is for batch pokemon API calls
-    # res = requests.get(url)
+
+    #this is for batch pokemon API calls
+    batch_url = "https://pokeapi.co/api/v2/pokemon?limit=10" 
     res = requests.get(batch_url)
 
     if res.status_code != 200:
         print(res.text)
     else:
+        # GET batch pokemon data to API
         data = res.json()
-        list_urls = [i for i in data['results']]
+
+        # make json object to list
+        list_urls = [i for i in data['results']]        #this contains the urls of each pokemon from batch GET
         final_data = []
         animated_gifs = []
+
+        # iterate over each urls
         for url in list_urls:
             response = requests.get(url['url'])
 
             if response.status_code != 200:
                 print(response.text)
             else:
+                # GET specific data of a pokemon
                 new_data = response.json()
-                animated_gif = new_data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'] #this is to extract the animated GIFs
-                final_data.append(new_data)
-                animated_gifs.append(animated_gif)
 
-        # print(final_data)
-        # print(animated_gifs)
-        # animated_gif = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+                #this is to extract the animated GIFs
+                animated_gif = new_data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']              
+
+                #append the pokemon data and GIFs to array
+                final_data.append(new_data)    
+                animated_gifs.append(animated_gif)      
        
+        # pass data as context object
         context = {
             'pokedata': final_data,
             'gifdata': animated_gifs
         }
         return render(request, 'pokemon/home.html', context)
+
+
+def load_pokedex(request):
+    
