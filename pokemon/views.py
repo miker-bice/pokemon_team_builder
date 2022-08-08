9b193799a2025bfa2d5e38c3edba6928d595358f
miker-bice <mikejerrson.galindez@gmail.com>
2022-08-08 14:50:50 +0800

    PokeAPI call now working

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 # this is the default home page
 def home(request):
-    return render(request, 'pokemon/home.html', {})
+    # try doing a call to the API
+    url = "https://pokeapi.co/api/v2/pokemon/pikachu"
+    res = requests.get(url)
+
+    if res.status_code != 200:
+        print(res.text)
+    else:
+        data = res.json()
+        context = {
+            'pokedata':data
+        }
+        return render(request, 'pokemon/home.html', context)
